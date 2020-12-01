9191a29810a313176aaaeb1b19800476fed9d46b
Wesley Norris <repnop@outlook.com>
2020-12-01 09:13:42 -0500

    Day 1, pretty short and sweet

--- src/day01.rs
+++ src/day01.rs
@@ -1,16 +1,27 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
 #[aoc_generator(day1)]
-fn parse_input(input: &str) -> u32 {
-    todo!()
+fn parse_input(input: &str) -> Vec<usize> {
+    input.lines().map(|s| s.trim().parse().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
-fn part1(vals: &u32) -> u32 {
-    todo!()
+fn part1(vals: &[usize]) -> usize {
+    vals.iter()
+        .copied()
+        .combinations(2)
+        .find(|v| v[0] + v[1] == 2020)
+        .map(|v| v[0] * v[1])
+        .unwrap()
 }
 
 #[aoc(day1, part2)]
-fn part2(vals: &u32) -> u32 {
-    todo!()
+fn part2(vals: &[usize]) -> usize {
+    vals.iter()
+        .copied()
+        .combinations(3)
+        .find(|v| v[0] + v[1] + v[2] == 2020)
+        .map(|v| v[0] * v[1] * v[2])
+        .unwrap()
 }
