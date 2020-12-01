4da43bb074d2d0ec199ec1886cadf653b116f7e7
Wesley Norris <repnop@outlook.com>
2020-11-30 19:54:51 -0500

    Initial commit prepped for day 1

---
+++ src/day01.rs
@@ -0,0 +1,16 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+fn parse_input(input: &str) -> u32 {
+    todo!()
+}
+
+#[aoc(day1, part1)]
+fn part1(vals: &u32) -> u32 {
+    todo!()
+}
+
+#[aoc(day1, part2)]
+fn part2(vals: &u32) -> u32 {
+    todo!()
+}
---
+++ src/main.rs
@@ -0,0 +1,1 @@
+aoc_runner_derive::aoc_main! { lib = advent_of_code_2020 }
